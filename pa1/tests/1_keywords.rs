892ac9fcb508a27e56df1743c6ce52662c89c95c
Chris Hathhorn <hathhorn@gmail.com>
2015-02-15 23:21:22 -0600

    Removed true/false from keywords, ' from symbols.

--- pa1/tests/1_keywords.rs
+++ pa1/tests/1_keywords.rs
@@ -2,12 +2,12 @@
 
 abstract alignof as be box
 break const continue crate do
-else enum extern false final
+else enum extern final
 fn for if impl in
 let loop macro macro_rules match mod
 move mut offsetof override priv
 pub pure ref return sizeof
-static self struct super true
+static self struct super
 trait type typeof unsafe unsized
 use virtual where while yield
 
