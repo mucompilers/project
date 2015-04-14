957b9d5f74d20b82cb5280e4cb4aba29976727d6
Chris Hathhorn <chathhorn@gmail.com>
2015-04-14 01:18:20 -0500

    Couple more tests.

---
+++ pa3/tests-p2/06-let.rs
@@ -0,0 +1,12 @@
+fn main() -> () {
+      let x : i32 = 3;
+      let y = x + 3;
+      let a = true;
+      let b : i32 = true;
+
+      x - y;
+      x - 3;
+      1 * y * 0;
+      a + x;
+      a / 3;
+}
